014a8c62232cc08099e23e6231c8d010c471dcd6
tim <tim.schoppe@gmx.net>
2022-06-20 17:44:06 +0200

    :)

--- src/com/processing/P4int.java
+++ src/com/processing/P4int.java
@@ -3,22 +3,17 @@ import processing.core.*;
 
 public class P4int extends PApplet {
 
-    public static void main(String[] args) {
+    public static void main(String[]args){
         PApplet.main("com.processing.P4int");
-
-
     }
 
     public void setup() {
         Picker.picken();
-
-
     }
 
 
     public void settings() {
         size(1000, 800);
-
     }
 
     public void draw() {
